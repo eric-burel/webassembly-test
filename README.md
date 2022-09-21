e71863ba01cdb8cade8f810c704857dba7158bf9
eric-burel <eb@lbke.fr>
2022-09-21 18:13:05 +0200

    setup gopls language server to use js, demo a js syscall

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-https://golangbot.com/webassembly-using-go/
+Done: https://golangbot.com/webassembly-using-go/
+Todo: https://golangbot.com/go-webassembly-dom-access/
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,15 @@
 package main
 
-import (  
-    "fmt"
-    "net/http"
+import (
+	"fmt"
+	"net/http"
 )
 
-func main() {  
-    err := http.ListenAndServe(":9090", http.FileServer(http.Dir("../../assets")))
-    if err != nil {
-        fmt.Println("Failed to start server", err)
-        return
-    }
+func main() {
+	fmt.Println("Will start server on http://localhost:9090")
+	err := http.ListenAndServe(":9090", http.FileServer(http.Dir("../../assets")))
+	if err != nil {
+		fmt.Println("Failed to start server", err)
+		return
+	}
 }
